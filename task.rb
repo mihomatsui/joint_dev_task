3dcea59c9d9447d89cd205b7f94cc5dfd5f904ec
mihomatsui <mihomatsui.24@gmail.com>
2020-12-29 00:55:52 +0900

    q16を修正

--- task.rb
+++ task.rb
@@ -149,8 +149,8 @@ def q16
   ]
 
   # 以下に回答を記載
-  users.each.with_index do |user,i|
-    puts "私の名前は#{users[i][:name]}です。年齢は#{users[i][:age]}歳です。"
+  users.each do |user|
+    puts "私の名前は#{user[:name]}です。年齢は#{user[:age]}歳です。"
   end
 end
 class UserQ17
