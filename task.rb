26ff180f254fec24672d48be594b7c93a6699ef8
mihomatsui <mihomatsui.24@gmail.com>
2021-01-13 12:36:24 +0900

    q18を回答

--- task.rb
+++ task.rb
@@ -178,7 +178,14 @@ end
 
 class UserQ18
   # 以下に回答を記載
+  def initialize(params)
+    @name = params[:name]
+    @age = params[:age]
+  end
 
+  def introduce
+    @age >= 20 ? "こんにちは,#{@name}と申します。宜しくお願いいたします。" : "はいさいまいど〜,#{@name}です!!!"
+  end
 end
 
 def q18
