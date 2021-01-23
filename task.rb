57c27dc13f9328be7b80dca1bda30141a4cf59c7
mihomatsui <mihomatsui.24@gmail.com>
2021-01-23 16:08:07 +0900

    q20 たまの入場料を変数で取り出し

--- task.rb
+++ task.rb
@@ -232,13 +232,14 @@ class Zoo
   def info_entry_fee(user)
     case user.age
     when 0..5
-      @entry_fee = 0
+      @entry_fee = @entry_fee[:infant]
     when 6..12
-      @entry_fee = 400
+      @entry_fee = @entry_fee[:children]
     when  13..64
-      @entry_fee = 800
+      @entry_fee = @entry_fee[:adult]
     when  65..120
-      @entry_fee = 500
+      @entry_fee = @entry_fee[:senior]
+
     end
     puts "#{user.name}さんの入場料金は #{@entry_fee} 円です。"
   end
